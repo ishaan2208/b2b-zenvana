4cbf8c8a24a777355859ecd5f623fac0fc7448b2
Ishaan Bajaj <bajaj.ishaan2208@gmail.com>
2024-04-30 14:53:04 +0530

    Add new configuration files and components

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,122 @@
+'use client'
+import Link from 'next/link'
+import { Button } from '@/components/Button'
+import { SelectField, TextField } from '@/components/Fields'
+import { Logo } from '@/components/Logo'
+import { SlimLayout } from '@/components/SlimLayout'
+import { type Metadata } from 'next'
+import { useState } from 'react'
+
+// export const metadata: Metadata = {
+//   title: 'Sign Up',
+// }
+
+export default function Register() {
+  // const form = useForm({})
+
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+
+  return (
+    <SlimLayout>
+      <div className="flex">
+        <Link href="/" aria-label="Home">
+          <Logo className="h-10 w-auto" />
+        </Link>
+      </div>
+      <h2 className="mt-20 text-lg font-semibold text-gray-900">
+        Get started for free
+      </h2>
+      {/* <p className="mt-2 text-sm text-gray-700">
+        Already registered?{' '}
+        <Link
+          href="/login"
+          className="font-medium text-blue-600 hover:underline"
+        >
+          Sign in
+        </Link>{' '}
+        to your account.
+      </p> */}
+      <form
+        action="#"
+        className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
+      >
+        <TextField
+          label="First name"
+          name="first_name"
+          type="text"
+          autoComplete="given-name"
+          required
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <TextField
+          label="Last name"
+          name="last_name"
+          type="text"
+          autoComplete="family-name"
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+          required
+        />
+        <TextField
+          className="col-span-full"
+          label="Phone number"
+          type="tel"
+          autoComplete="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        />
+        <TextField
+          className="col-span-full"
+          label="Email address"
+          type="email"
+          autoComplete="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        {/* <TextField
+          className="col-span-full"
+          label="Password"
+          name="password"
+          type="password"
+          autoComplete="new-password"
+          required
+        /> */}
+        {/* <SelectField
+          className="col-span-full"
+          label="How did you hear about us?"
+          name="referral_source"
+        >
+          <option>AltaVista search</option>
+          <option>Super Bowl commercial</option>
+          <option>Our route 34 city bus ad</option>
+          <option>The “Never Use This” podcast</option>
+        </SelectField> */}
+        <div className="col-span-full">
+          <Button
+            onClick={() => {
+              console.log('clicked')
+              console.log(firstname)
+              console.log(lastname)
+              console.log(phone)
+              console.log(email)
+            }}
+            type="submit"
+            variant="solid"
+            color="blue"
+            className="w-full"
+          >
+            <span>
+              Register <span aria-hidden="true">&rarr;</span>
+            </span>
+          </Button>
+        </div>
+      </form>
+    </SlimLayout>
+  )
+}
