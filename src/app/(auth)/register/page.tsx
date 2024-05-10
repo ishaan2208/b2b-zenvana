eb49476b8cb0d433411a3b7ca0c7905740ae87aa
Ishaan Bajaj <bajaj.ishaan2208@gmail.com>
2024-05-10 10:49:24 +0530

    Update Hero component with additional info call and phone number button

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -19,11 +19,13 @@ export default function Register() {
 
   const [firstname, setFirstname] = useState('')
   const [lastname, setLastname] = useState('')
+  const [loading, setLoading] = useState(false)
   const [phone, setPhone] = useState('')
   const [email, setEmail] = useState('')
   const router = useRouter()
 
   const onSubmit = (e: any) => {
+    setLoading(true)
     e.preventDefault()
     axios
       .post(`${process.env.NEXT_PUBLIC_API_URL}/leads`, {
@@ -40,6 +42,9 @@ export default function Register() {
         setEmail('')
         router.push('/')
       })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   return (
@@ -49,10 +54,24 @@ export default function Register() {
           <Logo className="h-24 w-auto" />
         </Link>
       </div>
-      <h2 className="mt-20 text-lg font-semibold text-gray-900">
-        Get started for free
-      </h2>
-      {/* <p className="mt-2 text-sm text-gray-700">
+      {loading ? (
+        <h1 className=" font-bold text-red-500">
+          Please do not refresh the page or go back form is getting submitted
+        </h1>
+      ) : (
+        <>
+          {' '}
+          <div className=" flex w-full flex-col items-center space-y-6">
+            <h2 className="mt-6 text-lg font-semibold text-gray-900">
+              Get started for free by filling the form or by calling us at{' '}
+            </h2>
+            <Button color="blue" className=" mt-4">
+              <a href="tel:+91 9084702208" className="text-white">
+                +91 9084702208
+              </a>
+            </Button>
+            <h1>Or</h1>
+            {/* <p className="mt-2 text-sm text-gray-700">
         Already registered?{' '}
         <Link
           href="/login"
@@ -62,47 +81,48 @@ export default function Register() {
         </Link>{' '}
         to your account.
       </p> */}
-      <form
-        action="#"
-        className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
-      >
-        <TextField
-          label="First name"
-          name="first_name"
-          type="text"
-          autoComplete="given-name"
-          required
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
-        />
-        <TextField
-          label="Last name"
-          name="last_name"
-          type="text"
-          autoComplete="family-name"
-          value={lastname}
-          onChange={(e) => setLastname(e.target.value)}
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Phone number"
-          type="tel"
-          autoComplete="tel"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          required
-        />
-        <TextField
-          className="col-span-full"
-          label="Email address"
-          type="email"
-          autoComplete="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        {/* <TextField
+
+            <form
+              action="#"
+              className="mt-10 grid w-full grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
+            >
+              <TextField
+                label="First name"
+                name="first_name"
+                type="text"
+                autoComplete="given-name"
+                required
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+              <TextField
+                label="Last name"
+                name="last_name"
+                type="text"
+                autoComplete="family-name"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+                required
+              />
+              <TextField
+                className="col-span-full"
+                label="Phone number"
+                type="tel"
+                autoComplete="tel"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+              <TextField
+                className="col-span-full"
+                label="Email address"
+                type="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              {/* <TextField
           className="col-span-full"
           label="Password"
           name="password"
@@ -110,7 +130,7 @@ export default function Register() {
           autoComplete="new-password"
           required
         /> */}
-        {/* <SelectField
+              {/* <SelectField
           className="col-span-full"
           label="How did you hear about us?"
           name="referral_source"
@@ -120,20 +140,23 @@ export default function Register() {
           <option>Our route 34 city bus ad</option>
           <option>The “Never Use This” podcast</option>
         </SelectField> */}
-        <div className="col-span-full">
-          <Button
-            onClick={onSubmit}
-            type="submit"
-            variant="solid"
-            color="blue"
-            className="w-full"
-          >
-            <span>
-              Register <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Button>
-        </div>
-      </form>
+              <div className="col-span-full">
+                <Button
+                  onClick={onSubmit}
+                  type="submit"
+                  variant="solid"
+                  color="blue"
+                  className="w-full"
+                >
+                  <span>
+                    Register <span aria-hidden="true">&rarr;</span>
+                  </span>
+                </Button>
+              </div>
+            </form>
+          </div>
+        </>
+      )}
     </SlimLayout>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,6 +27,26 @@ export function Hero() {
         </span>{' '}
         {/* for superior staying experiences. */}
       </h1>
+      <h1 className=" mt-16 uppercase">
+        For{' '}
+        <span className="relative whitespace-nowrap text-blue-600">
+          <svg
+            aria-hidden="true"
+            viewBox="0 0 418 42"
+            className="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70"
+            preserveAspectRatio="none"
+          >
+            <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
+          </svg>
+          <span className="relative">more</span>
+        </span>{' '}
+        info call
+      </h1>
+      <Button color="blue" className=" mt-4">
+        <a href="tel:+91 9084702208" className="text-white">
+          +91 9084702208
+        </a>
+      </Button>
       <p className="mx-auto mt-12 max-w-5xl text-lg tracking-tight text-slate-700">
         StaySystems is designed to revolutionize property management by
         empowering your front office team with intuitive tools that simplify
