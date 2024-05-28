6d120bd66d1c177b4bb497a7b19eb0c80f3c0a45
Ishaan Bajaj <bajaj.ishaan2208@gmail.com>
2024-05-28 16:46:08 +0530

    Update CallToAction and Hero components with revised trial period and consistent formatting

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -24,11 +24,11 @@ export function CallToAction() {
             Get started today
           </h2>
           <p className="mt-4 text-lg tracking-tight text-white">
-            It’s time to take control of your books. Buy our software so you can
+            It’s time to take control of your Hotel. Buy our software so you can
             feel like you’re doing something productive.
           </p>
           <Button href="/register" color="white" className="mt-10">
-            Get 15 days free trial
+            Get 3 months free
           </Button>
         </div>
       </Container>
